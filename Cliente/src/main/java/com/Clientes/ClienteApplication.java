fbd77add747daad2b5bbb59191ff17c1354e3aab
natanael reveco <reveconatanael03@gmail.com>
2025-06-09 23:17:25 -0400

    se modificaron la api clientes

--- Cliente/src/main/java/com/Clientes/ClienteApplication.java
+++ Cliente/src/main/java/com/Clientes/ClienteApplication.java
@@ -1,13 +1,14 @@
-package com.Cliente;
+package com.Clientes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.clientes"})
 public class ClienteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClienteApplication.class, args);
 	}
+	
 
 }
