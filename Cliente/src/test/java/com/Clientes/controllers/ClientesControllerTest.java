7d312f197a1ff848944f4c29bd4f040b09988d33
Dante Schacht <dantesch10@gmail.com>
2025-07-01 20:34:34 -0400

    Se agrego el Test con Junit al controller

---
+++ Cliente/src/test/java/com/Clientes/controllers/ClientesControllerTest.java
@@ -0,0 +1,30 @@
+package com.Clientes.controllers;
+
+import org.junit.jupiter.api.Test;
+
+public class ClientesControllerTest {
+    @Test
+    void testActualizar() {
+
+    }
+
+    @Test
+    void testCrear() {
+
+    }
+
+    @Test
+    void testEliminar() {
+
+    }
+
+    @Test
+    void testListar() {
+
+    }
+
+    @Test
+    void testObtener() {
+
+    }
+}
